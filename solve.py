34be15d7991360224365bc3611410576b5f6dc3d
Victor Leung <yck011522@gmail.com>
2022-09-28 21:55:05 +0200

    Bug fix for incorrect shortcut logic

--- solve.py
+++ solve.py
@@ -3,7 +3,7 @@ import json
 from copy import deepcopy
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--file", default='image/567.json', help="Path to the analyzed colour group.json")
+ap.add_argument("-f", "--file", default='image/595.json', help="Path to the analyzed colour group.json")
 args = vars(ap.parse_args())
 
 uniq_colors, group_lists = None, None
@@ -14,6 +14,7 @@ with open(args["file"], "r") as f:
 print("Loaded {} Lists, {} Unique Colors".format(len(group_lists), len(uniq_colors)))
 assert len(group_lists) == len(uniq_colors)
 
+# Global properties about the game board
 columns = len(group_lists) + 2
 height = len(group_lists[0])
 initial_board_state = deepcopy(group_lists) + [[], []]
@@ -21,6 +22,7 @@ initial_board_state = deepcopy(group_lists) + [[], []]
 # Implementation to keep track of visited state to avoid unnecessary re-trying
 visited_state_hash = set()
 hash_len = 0
+deepest_search = 0
 
 
 def state_hash_default(state):
@@ -74,7 +76,7 @@ def perform_action(board_state, column_fm, column_to):
     if len(board_state[column_to]) > 0 and empty_space < height:
         target_color = board_state[column_to][-1]
         if len(board_state[column_fm]) > empty_space:
-            if board_state[column_fm][-empty_space] == target_color:
+            if board_state[column_fm][-empty_space - 1] == target_color:
                 return None
 
     # Valid Action
@@ -102,11 +104,9 @@ def column_complete(column):
 def find_possibilities(board_state):
     actions = []
     for i in range(0, columns):
-        # Filter out columns that are already complete
-        if column_complete(board_state[i]):
-            continue
         for j in range(0, columns):
             if column_complete(board_state[j]):
+                # Filter out columns that are already complete
                 continue
             if i == j:
                 continue
@@ -125,12 +125,20 @@ def is_solved(board_state):
 
 
 def search(actions, board_states, verbose=0):
-    if verbose >= len(actions):
-        print(actions)
-        print("Last State:", board_states[-1])
-    if len(actions) > 100:
+    global deepest_search
+
+    # Keep statistics
+    depth = len(actions)
+    if depth > deepest_search:
+        deepest_search = depth
+    # Avoid Infinite Loop
+    if depth > 100:
         print("Action Stuck?")
         return None
+
+    explored_actions = 0
+
+    # Iterate through possible actions
     for (po_action, po_new_state) in find_possibilities(board_states[-1]):
         # Check if this actions results in a solved board
         if is_solved(po_new_state):
@@ -144,14 +152,20 @@ def search(actions, board_states, verbose=0):
             continue
 
         # Commit to this action and continue to search forward
+        explored_actions += 1
         result = search(actions + [po_action], board_states + [po_new_state], verbose=verbose)
         if result is not None:
             deeper_actions, deeper_states = result
             return ([po_action] + deeper_actions, [po_new_state] + deeper_states)
+
+    if verbose >= depth:
+        print("Depth = {} , Explored Options = {} , hash_len = {} , actions = {}".format(depth, explored_actions, hash_len, actions))
+        print("Last State:", board_states[-1])
+
     return None
 
 
-verbose = 5
+verbose = 20
 result = search([], [initial_board_state], verbose)
 if result is not None:
     actions, board_states = result
@@ -160,3 +174,4 @@ if result is not None:
     print("hash_len:", hash_len)
 else:
     print("Solution not found")
+    print("hash_len:", hash_len)
